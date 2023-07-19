f4b6dd15854b0971ac814ec130edbf40fa0b949c
or-abdillh <hans.abdillh05@gmail.com>
2023-07-20 00:22:48 +0800

    Update Athlet section

--- src/pages/partials/AthletSection.vue
+++ src/pages/partials/AthletSection.vue
@@ -1,29 +1,9 @@
 <template>
     <SectionCard data-role="main-section" id="Atlet">
         <SectionTitle title="Zone Of Athlet" subtitle="Melampaui Batas Prestasi dan Semangat Dalam Dunia Muay Thai"></SectionTitle>
-        <section class="">
-            <!-- subs menu -->
-            <template v-for="(sub, x) in subs" :key="x">
-                <div class="w-full relative bg-[url('/images/muay-thai-battle.jpg')] bg-center bg-cover bg-no-repeat mb-6 rounded-xl overflow-hidden">
-                    <div class="bg-slate-900 bg-opacity-90 hover:backdrop-blur-lg duration-300 p-12 flex flex-col md:flex-row md:items-center gap-6">
-                        <!-- icon -->
-                        <div>
-                            <div class="w-20 h-20 bg-orange-400 rounded-lg grid place-items-center">
-                                <i class="fa-solid text-slate-200 text-xl" :class="sub?.icon"></i>
-                            </div>
-                        </div>
-                        <!-- copywriter -->
-                        <div class="md:w-9/12">
-                            <p class="text-2xl text-slate-200 font-semibold mb-2">{{ sub.heading }}</p>
-                            <p class="text-slate-300">{{ sub.sub }}</p>
-                        </div>
-                        <!-- link -->
-                        <div class="md:text-center flex-grow">
-                            <a href="" class="border border-slate-300 text-slate-300 px-6 py-2 text-sm hover:bg-orange-600 duration-300">Klik Disini</a>
-                        </div>
-                    </div>
-                </div>
-            </template>
+        
+        <section>
+            <AthletStaticticSection></AthletStaticticSection>
         </section>
     </SectionCard>
 </template>
@@ -32,11 +12,12 @@
 
 import SectionCard from '@component/base/SectionCard.vue'
 import SectionTitle from '@component/base/SectionTitle.vue'
+import AthletStaticticSection from '@page/partials/AthletStaticticSection.vue'
 
 const subs = [
     {
         heading: 'Statistik Partisipasi dan Prestasi Atlet di Indonesia',
-        sub: 'Mengungkap Kisah Gemilang dan Dedikasi Atlet Muay Thai Indonesia: Menelusuri Sejarah Partisipasi dalam Event Bergengsi dan Meraih Prestasi Gemilang di Tingkat Nasional dan Internasional',
+        sub: '',
         icon: 'fa-trophy'
     },
     {
---
+++ src/pages/partials/AthletStaticticSection.vue
@@ -0,0 +1,189 @@
+ <template>
+    <section>
+        <!-- wrapper -->
+        <div class="flex flex-col md:flex-row gap-6 justify-between items-center">
+            <!-- copywriter -->
+            <div class="md:w-5/12">
+                <p class="font-semibold text-3xl text-slate-200 mb-4">
+                    <!-- icon -->
+                    <i class="fa-solid fa-trophy me-2"></i>
+                    Statistik Partisipasi dan Prestasi Atlet di Indonesia
+                </p>
+                <p class="text-lg text-slate-300">Mengungkap Kisah Gemilang dan Dedikasi Atlet Muay Thai Indonesia: Menelusuri Sejarah Partisipasi dalam Event Bergengsi dan Meraih Prestasi Gemilang di Tingkat Nasional dan Internasional</p>
+            </div>
+            <!-- stats -->
+            <div class="md:w-6/12">
+                <!-- bar -->
+                <div class="flex flex-wrap md:flex-nowrap text-slate-300 mb-1">
+                    <template v-for="{ year } in statistics" :key="year">
+                        <button @click="yearActive = year" :class="{'border-b border-slate-200 bg-orange-600': yearActive == year, 'bg-orange-950': yearActive != year}" class="px-9 py-2 duration-300 hover:bg-orange-900">{{ year }}</button>
+                    </template>
+                </div>
+                <!-- lists -->
+                <div class="flex flex-wrap md:flex-nowrap gap-1 mb-6">
+                    <template v-for="stat in statistics" :key="stat">
+                        <!-- participants -->
+                        <div v-if="stat?.year == yearActive" class="bg-orange-600 text-slate-200 p-8 text-center text-sm flex-grow">
+                            <h1 class="text-slate-300 text-3xl mb-2">
+                                <i class="fa-solid fa-user text-2xl"></i>
+                                {{ stat?.totalParticipants }} 
+                            </h1>
+                            Partisipasi Atlet
+                        </div>
+                        <!-- gold -->
+                        <div v-if="stat?.year == yearActive" class="bg-orange-600 text-slate-200 p-8 text-center text-sm flex-grow">
+                            <h1 class="text-slate-300 text-3xl mb-2">
+                                <i class="fa-solid fa-medal text-2xl"></i>
+                                {{ stat?.goldMedals }}</h1>
+                            Emas
+                        </div>
+                        <!-- silver -->
+                        <div v-if="stat?.year == yearActive" class="bg-orange-600 text-slate-200 p-8 text-center text-sm flex-grow">
+                            <h1 class="text-slate-300 text-3xl mb-2">
+                                <i class="fa-solid fa-medal text-2xl"></i>
+                                {{ stat?.silverMedals }}
+                            </h1>
+                            Silver
+                        </div>
+                        <!-- gold -->
+                        <div v-if="stat?.year == yearActive" class="bg-orange-600 text-slate-200 p-8 text-center text-sm flex-grow">
+                            <h1 class="text-slate-300 text-3xl mb-2">
+                                <i class="fa-solid fa-medal text-2xl"></i>
+                                {{ stat?.bronzeMedals }}
+                            </h1>
+                            Perunggu
+                        </div>
+                    </template>
+                </div>
+                <!-- tournaments -->
+                <div>
+                    <p class="text-slate-300 mb-2">Turnamen Internasional</p>
+                    <div class="flex flex-col md:flex-row md:gap-3">
+                        <template v-for="stat in statistics" :key="stat.internationalTournaments">
+                            <template v-for="tournament in stat.internationalTournaments" :key="tournament">
+                                <div v-if="stat?.year == yearActive" class="mb-2">
+                                    <p class="text-slate-200 text-sm">
+                                        <i class="fa-solid fa-medal me-2" :class="{ 'text-yellow-500': tournament?.medalType == 'gold', 'text-slate-500': tournament?.medalType == 'silver', 'text-orange-800': tournament?.medalType == 'bronze' }"></i>
+                                        {{ tournament?.tournamentName }}
+                                    </p>
+                                </div>
+                            </template>
+                        </template>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+import { ref } from 'vue'
+
+const yearActive = ref('2021')
+
+const statistics = [
+    {
+        year: 2021,
+        totalParticipants: 120,
+        goldMedals: 10,
+        silverMedals: 15,
+        bronzeMedals: 20,
+        totalMedals: 45,
+        nationalChampionship: true,
+        internationalTournaments: [
+            {
+                tournamentName: "Asian Muay Thai Championships",
+                medalType: "gold",
+            },
+            {
+                tournamentName: "SEA Games",
+                medalType: "silver",
+            },
+            {
+                tournamentName: "World Muay Thai Championships",
+                medalType: "bronze",
+            },
+        ],
+    },
+    {
+        year: 2020,
+        totalParticipants: 100,
+        goldMedals: 5,
+        silverMedals: 10,
+        bronzeMedals: 15,
+        totalMedals: 30,
+        nationalChampionship: true,
+        internationalTournaments: [
+            {
+                tournamentName: "Asian Muay Thai Championships",
+                medalType: "silver",
+            },
+            {
+                tournamentName: "SEA Games",
+                medalType: "bronze",
+            },
+        ],
+    },
+    {
+        year: 2019,
+        totalParticipants: 80,
+        goldMedals: 7,
+        silverMedals: 12,
+        bronzeMedals: 18,
+        totalMedals: 37,
+        nationalChampionship: true,
+        internationalTournaments: [
+            {
+                tournamentName: "Asian Muay Thai Championships",
+                medalType: "gold",
+            },
+        ],
+    },
+    {
+        year: 2018,
+        totalParticipants: 90,
+        goldMedals: 9,
+        silverMedals: 14,
+        bronzeMedals: 22,
+        totalMedals: 45,
+        nationalChampionship: true,
+        internationalTournaments: [
+            {
+                tournamentName: "SEA Games",
+                medalType: "silver",
+            },
+            {
+                tournamentName: "World Muay Thai Championships",
+                medalType: "bronze",
+            },
+        ],
+    },
+    {
+        year: 2017,
+        totalParticipants: 70,
+        goldMedals: 6,
+        silverMedals: 11,
+        bronzeMedals: 15,
+        totalMedals: 32,
+        nationalChampionship: true,
+        internationalTournaments: [
+            {
+                tournamentName: "Asian Muay Thai Championships",
+                medalType: "silver",
+            },
+        ],
+    },
+    {
+        year: 2016,
+        totalParticipants: 60,
+        goldMedals: 4,
+        silverMedals: 8,
+        bronzeMedals: 12,
+        totalMedals: 24,
+        nationalChampionship: true,
+        internationalTournaments: [],
+    },
+]
+
+</script>
